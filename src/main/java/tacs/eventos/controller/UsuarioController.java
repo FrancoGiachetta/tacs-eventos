d241741a1f7ba15d2ec0ee67e53844bd0d1f1b0d
Lucas Thier <lucasthier2015@gmail.com>
2025-09-01 13:55:39 -0300

    endpoint mis eventos

--- src/main/java/tacs/eventos/controller/UsuarioController.java
+++ src/main/java/tacs/eventos/controller/UsuarioController.java
@@ -2,32 +2,44 @@ package tacs.eventos.controller;
 
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tacs.eventos.dto.InscripcionEventoDTO;
+import tacs.eventos.model.Evento;
 import tacs.eventos.model.Usuario;
+import tacs.eventos.repository.evento.EventosRepository;
 import tacs.eventos.service.UsuarioService;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/usuarios")
+@RequestMapping("/api/v1/usuario")
 @AllArgsConstructor
-public class UsuarioController {
+public class UsuarioController
+{
 
     private final UsuarioService usuarioService;
-    private final ModelMapper modelMapper;
+    private final EventosRepository eventosRepository;
 
-    @GetMapping("/usuarios/{email}")
-    public Optional<Usuario> getUsuario(@PathVariable String email) {
+    @GetMapping("/{email}") // todo eliminar este endpoint
+    public Optional<Usuario> getUsuario(@PathVariable String email)
+    {
         return usuarioService.buscarPorEmail(email);
     }
 
-    @GetMapping("/mis-inscripciones/{userId}")
-    public List<InscripcionEventoDTO> getMisInscripciones(@PathVariable String userId) {
-        return usuarioService.obtenerInscripciones(userId);
+    @GetMapping("/mis-inscripciones")
+    public List<InscripcionEventoDTO> getMisInscripciones(@AuthenticationPrincipal Usuario usuario)
+    {
+        return usuarioService.obtenerInscripciones(usuario.getId());
+    }
+
+    @GetMapping("/mis-eventos")
+    public List<Evento> getMisEventos(@AuthenticationPrincipal Usuario usuario)
+    {
+        return eventosRepository.getEventosPorOrganizador(usuario.getId());
     }
 }
