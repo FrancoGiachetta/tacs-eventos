4fdb3c73bc7b65aeba320847b9bd9ff158a14bef
FrancoGiachetta <francogiachetta27@gmail.com>
2025-10-05 14:06:01 -0300

    formato

--- backend/src/main/java/tacs/eventos/dto/errores/ErrorResponse.java
+++ backend/src/main/java/tacs/eventos/dto/errores/ErrorResponse.java
@@ -2,9 +2,7 @@ package tacs.eventos.dto.errores;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(
-        String mensaje,
-        LocalDateTime momento) {
+public record ErrorResponse(String mensaje, LocalDateTime momento) {
 
     public ErrorResponse(String mensaje) {
         this(mensaje, LocalDateTime.now());
