bd89f2a5fcea36a5839628cba265311dcf13634c
crenfigeUtn <129232585+crenfigeUtn@users.noreply.github.com>
2025-10-06 12:52:39 -0300

    formato BE FE

--- backend/src/test/java/tacs/eventos/EventosApplicationTests.java
+++ backend/src/test/java/tacs/eventos/EventosApplicationTests.java
@@ -5,10 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import tacs.eventos.config.TestMongoConfiguration;
 import tacs.eventos.config.TestRedisConfiguration;
-import tacs.eventos.config.TestSessionConfiguration;
 
 @SpringBootTest
-@Import({ TestRedisConfiguration.class, TestMongoConfiguration.class, TestSessionConfiguration.class})
+@Import({ TestRedisConfiguration.class, TestMongoConfiguration.class })
 class EventosApplicationTests {
 
     @Test
